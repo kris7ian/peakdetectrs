3e43639020423aae0629c50791c6532970f1713d
kristian <kristian@kristians-MacBook-Pro.local>
2022-07-24 20:43:32 +0200

    Clean up and minor changes

--- src/lib.rs
+++ src/lib.rs
@@ -5,28 +5,16 @@ fn detect_peaks(
     array: Vec<f64>,
     lookahead: usize,
 ) -> PyResult<(Vec<(usize, f64)>, Vec<(usize, f64)>)> {
+
     let mut max_peaks: Vec<(usize, f64)> = Vec::new();
     let mut min_peaks: Vec<(usize, f64)> = Vec::new();
     let mut rolling_minimum: f64 = f64::INFINITY;
     let mut rolling_maximum: f64 = f64::NEG_INFINITY;
     let _delta = 0;
-
     let end = array.len() - lookahead;
 
     for (index, y) in array[..end].iter().enumerate() {
         let is_too_early = index < lookahead;
-
-        //let look_for_maximum = rolling_maximum != f64::INFINITY;
-        //let look_for_minimum = rolling_minimum != f64::NEG_INFINITY;
-
-        // println!("Is_too_early: {}", is_too_early);
-        // println!("Looking for max: {}", look_for_maximum);
-        // println!("Looking for min: {}", look_for_minimum);
-        // println!("index: {}", index);
-        // println!("max: {}", rolling_maximum);
-        // println!("min: {}", rolling_minimum);
-        // println!("y: {}", y);
-        // println!("-----------");
 
         if y >= &rolling_maximum {
             rolling_maximum = y.clone();
@@ -61,7 +49,7 @@ fn detect_peaks(
     return Ok((max_peaks, min_peaks));
 }
 
-/// A Python module implemented in Rust.
+
 #[pymodule]
 fn peakdetectrs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(detect_peaks, m)?)?;
